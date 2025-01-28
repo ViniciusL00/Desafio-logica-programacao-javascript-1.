45fdc241f77a002351cd249adf2d0dcf00ee08ad
Vinicius Leme <vinicius.leme00@gmail.com>
2025-01-28 17:13:00 -0300

    Alert
    Alert corrigido

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 alert ('Bem-vindo ao nosso site!');
 
-let nome = "Lua";
+let nome = "Jingles";
 let idade = 25;
 
 let numeroDeVendas = 50;
 let saldoDisponivel = 1000;
 
-let mensagemDeErro = 'Erro, preencha todos os campos';
-alert (mensagemDeErro);
-
 nome= prompt('Digite o seu nome');
-if (nome == "Lua") {
+
+if (nome == "Jingles") {
     alert('Pode tirar a habilitação!');
-}else{
+}else {
     alert('Não pode tirar a habilitação!');
 }
 
 idade= prompt('Digite a sua idade');
+
 if (idade >= 18) {
-    ('Pode tirar habilitação!');
+    alert('Pode tirar habilitação!');
 }else {
     alert('Não pode tirar habilitação!');
 }
