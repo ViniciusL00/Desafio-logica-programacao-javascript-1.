62a54465f71a569d1a26cc4ae633864ef74ebc60
Vinicius Leme <vinicius.leme00@gmail.com>
2025-01-28 15:59:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-alert('Bem-vindo ao nosso site!');
+alert ('Bem-vindo ao nosso site!');
 
 let nome = "Lua";
 let idade = 25;
+
 let numeroDeVendas = 50;
 let saldoDisponivel = 1000;
+alert('Erro, Preencha todos os campos');
 
-alert ('Erro! Preencha todos os campos');
-
-let mensagemDeErro = 'Erro! Preencha todos os campos';
-alert(mensagemDeErro);
+let mensageDeErro = 'Erro, preencha todos os campos';
+alert (mensagemDeErro);
 
 let nome = prompt('Digite o seu nome');
-let idade = prompt('Digite sua idade');
+let idade = prompt('Digite a sua idade');
 
-let idade = prompt('Digite sua idade');
+let = prompt('Digite a sua idade');
 if (idade >= 18) {
     alert('Pode tirar a habilitação!');
 }
